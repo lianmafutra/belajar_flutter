4db5c8eb4a643dce3668b5d687ffc5fd92d4b069
lianmafutra <lianmafutra@gmail.com>
2020-12-21 00:45:07 +0700

    fix

--- belajar/lib/silapor/lib/report_detail.dart
+++ belajar/lib/silapor/lib/report_detail.dart
@@ -48,28 +48,27 @@ class _ReportDetailState extends State<ReportDetail> {
             onPressed: () => Navigator.of(context).pop(),
           ),
         ),
+        
         body: Container(
-          padding: EdgeInsets.all(20),
+           padding: EdgeInsets.all(20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            
+             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Flexible(
-                  child: ListView(children: <Widget>[
-                Flexible(
-                    child: Container(
+           Flexible(child: ListView(
+             children: <Widget>[
+                 Container(
                   padding: EdgeInsets.only(bottom: 6),
                   child: Text(widget.title,
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                )),
-                Flexible(
-                    child: Container(
+                ),
+                 Container(
                   child: Text("Tanggal Lapor : " + widget.createdAt,
                       style: TextStyle(fontSize: 14)),
-                )),
-                Flexible(
-                  child: Container(
+                ),
+                Container(
                       padding: EdgeInsets.only(top: 20),
                       child: Material(
                           child: InkWell(
@@ -101,15 +100,82 @@ class _ReportDetailState extends State<ReportDetail> {
                                   )),
                         ),
                       ))),
-                ),
-                Flexible(
-                    child: Container(
+                      Container(
                         padding: EdgeInsets.only(top: 26),
                         child: Text(widget.content,
-                            style: TextStyle(fontSize: 16)))),
-              ]))
-            ],
-          ),
+                            style: TextStyle(fontSize: 16)))
+             ],
+           ))
+           
+            
+          ],),
+          
+        //   padding: EdgeInsets.all(20),
+          
+          
+        //   child: 
+          
+          
+        //   Column(
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: <Widget>[
+        //       Flexible(
+        //           child: ListView(children: <Widget>[
+        //         Flexible(
+        //             child: Container(
+        //           padding: EdgeInsets.only(bottom: 6),
+        //           child: Text(widget.title,
+        //               style:
+        //                   TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        //         )),
+        //         Flexible(
+        //             child: Container(
+        //           child: Text("Tanggal Lapor : " + widget.createdAt,
+        //               style: TextStyle(fontSize: 14)),
+        //         )),
+        //         Flexible(
+        //           child: Container(
+        //               padding: EdgeInsets.only(top: 20),
+        //               child: Material(
+        //                   child: InkWell(
+        //                 onTap: () {
+        //                   Navigator.push(context,
+        //                       MaterialPageRoute(builder: (_) {
+        //                     return DetailScreen(img_url: widget.image);
+        //                   }));
+        //                 },
+        //                 child: Container(
+        //                   // child:  Image.network(widget.image,fit: BoxFit.fill ),
+        //                   child: showImageWidget
+        //                       ? Image.network(
+        //                           widget.image,
+        //                           width: MediaQuery.of(context).size.width,
+        //                           height: 400.0,
+        //                           fit: BoxFit.fill,
+        //                         )
+        //                       : SizedBox(
+        //                           width: MediaQuery.of(context).size.width,
+        //                           height: 400.0,
+        //                           child: Shimmer.fromColors(
+        //                             child: Card(
+        //                               color: Colors.grey,
+        //                             ),
+        //                             baseColor: Colors.grey[300],
+        //                             highlightColor: Colors.grey[100],
+        //                             direction: ShimmerDirection.ltr,
+        //                           )),
+        //                 ),
+        //               ))),
+        //         ),
+        //         Flexible(
+        //             child: Container(
+        //                 padding: EdgeInsets.only(top: 26),
+        //                 child: Text(widget.content,
+        //                     style: TextStyle(fontSize: 16)))),
+        //       ]))
+        //     ],
+        //   ),
         ),
       ),
     );
@@ -117,7 +183,7 @@ class _ReportDetailState extends State<ReportDetail> {
 }
 
 class DetailScreen extends StatelessWidget {
-  String img_url;
+  final String img_url;
   DetailScreen({Key key, this.img_url}) : super(key: key);
 
   @override
